5e1fa608d4cca940ccce7e6ca86d411a3524c117
Ashish Kumar <akdev5520@gmail.com>
2024-02-21 19:23:20 +0530

    removed redundant comments

--- components/info.tsx
+++ components/info.tsx
@@ -58,13 +58,3 @@ const Info: React.FC<InfoProps> = ({ data }) => {
 };
 
 export default Info;
-
-// {data?.color?.value !== "purple" && (
-// 	<div className="flex items-center gap-x-4">
-// 		<h3 className="font-semibold text-black">Color:</h3>
-// 		<div
-// 			className="h-6 w-6 rounded-full border border-gray-600"
-// 			style={{ backgroundColor: data?.color?.value }}
-// 		/>
-// 	</div>
-// )}
