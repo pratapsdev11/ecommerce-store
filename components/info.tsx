32947e75e58e2c3f084978ef58f39dfe4c4014dc
Ashish Kumar <akdev5520@gmail.com>
2024-02-21 19:22:44 +0530

    added conditional rendering of color value in info.tsx

--- components/info.tsx
+++ components/info.tsx
@@ -33,11 +33,15 @@ const Info: React.FC<InfoProps> = ({ data }) => {
 					<div>{data?.size?.value}</div>
 				</div>
 				<div className="flex items-center gap-x-4">
-					<h3 className="font-semibold text-black">Color:</h3>
-					<div
-						className="h-6 w-6 rounded-full border border-gray-600"
-						style={{ backgroundColor: data?.color?.value }}
-					/>
+					{data?.color?.value == "null" && (
+						<>
+							<h3 className="font-semibold text-black">Color:</h3>
+							<div
+								className="h-6 w-6 rounded-full border border-gray-600"
+								style={{ backgroundColor: data?.color?.value }}
+							/>
+						</>
+					)}
 				</div>
 			</div>
 			<div className="mt-10 flex items-center gap-x-3">
@@ -54,3 +58,13 @@ const Info: React.FC<InfoProps> = ({ data }) => {
 };
 
 export default Info;
+
+// {data?.color?.value !== "purple" && (
+// 	<div className="flex items-center gap-x-4">
+// 		<h3 className="font-semibold text-black">Color:</h3>
+// 		<div
+// 			className="h-6 w-6 rounded-full border border-gray-600"
+// 			style={{ backgroundColor: data?.color?.value }}
+// 		/>
+// 	</div>
+// )}
